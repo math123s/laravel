b480954ea4c44cb887fb0494c7ac1ab57c7f144d
math123s <matheusvpenteado11@gmail.com>
2026-03-03 14:48:14 -0300

    03-03-26-matheus-vieira

--- app/Http/Controllers/Principal.php
+++ app/Http/Controllers/Principal.php
@@ -1,5 +1,5 @@
 <?php
-
+// Nome do espaço aonde estou espaço, controllers é a pasta pai, ou nome da região
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
@@ -7,8 +7,25 @@ use Illuminate\Http\Request;
 class Principal extends Controller
 {
     function principal(){
-        echo 'Página Principal';
+        return view('pagina-principal');
     }
+
+    function sobre(){
+        return view('sobre');
+    }
+    function contato(){
+        return view('contato');
+    }
+    function desenvolvimentoSistemas(){
+        return view('desenvolvimentoSistemas');
+    }
+    function administracao(){
+        return view('administracao');
+    }
+    function meioAmbiente(){
+        return view('meio-ambiente');
+    }
+    
 
     
 }
--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,12 @@ use App\Http\Middleware\LogAcessoMiddleware;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get('/', [App\Http\Controllers\Principal::class, 'principal']);
-
-
-
+//       raiz  metodos http Pesquisar
+Route::get('/', [App\Http\Controllers\Principal::class, 'principal'])->name('principal');
+Route::get('/sobre', [App\Http\Controllers\Principal::class, 'sobre'])->name('sobre');
+Route::get('/contato', [App\Http\Controllers\Principal::class, 'contato'])->name('contato');
+//tem que ser toLowcase pq é fução
+Route::get('/desenvolvimento-sistemas', [App\Http\Controllers\Principal::class, 'desenvolvimentoSistemas'])->name('desenvolvimentoSistemas');
+Route::get('/administracao', [App\Http\Controllers\Principal::class, 'administracao'])->name('administracao');
+Route::get('/meio-ambiente', [App\Http\Controllers\Principal::class, 'meioAmbiente'])->name('meioAmbiente');
 
